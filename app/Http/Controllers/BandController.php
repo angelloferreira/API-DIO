5af4401e11410f9ae83905a3104726031247ad24
Angelo Ferreira Guimaraes <01832420256@rondonia.local>
2023-12-11 08:05:00 -0400

    Commit inicial

---
+++ app/Http/Controllers/BandController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class BandController extends Controller
+{
+    public function getAll()
+    {
+        return response()->json([]);
+    }
+}
+
